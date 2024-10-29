b50447dd0dca4586626051ef4d00519776d644f2
Tosin Akinnusi <tosin.akinnusi@asseco.ng>
2024-10-29 16:29:23 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LeaveManagementSystem.Web.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+    }
+}
---
+++ LeaveManagementSystem.Web/Data/Models/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data.Models
+{
+    public class LeaveType
+    {
+        public long Id { get; set; }
+
+        [MaxLength(150, ErrorMessage = "lenth cannot be more than 150 characters")]
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+
+    }
+}
